b873c1b1286cdf31cc37543dfe428ef4e36bb038
Dhruvan <dhruvan.k05@gmail.com>
2024-02-16 00:39:41 -0500

    Delete unused object files

--- build.py
+++ build.py
@@ -7,14 +7,27 @@ if "build_exe" not in sys.argv:
     subprocess.run([sys.executable, os.path.abspath(__file__), "build_exe"])
     sys.exit(0)
 
-build_exe_options = {
-    "zip_include_packages": ["encodings", "PySide6", "shiboken6"],
-}
-
 setup(
     name="helix",
     version="0.1",
+    author="Dhruvan Kartik",
+    author_email="ze7111@gmail.com",
+    license="CC0 1.0 Universal",
     description="Helix is a programming language that is designed to be simple and easy to use",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("start.py")],
+    options={
+        "build_exe": {
+            "packages": ["os", "sys", "subprocess", "re", "threading", "shutil", "json", "time", "argparse"],
+        },
+        "optimize": 2,
+        "include_msvcr": True
+        
+    },
+    
+    executables=[
+        Executable(
+            "start.py",
+            icon="helix.ico",
+            
+        )
+    ],
 )
