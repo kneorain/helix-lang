2016e9ca235eae09af0cc8fd0dda66798079cc3d
Joshua Carlson <61999256+jrcarl624@users.noreply.github.com>
2024-03-24 15:34:57 -0400

    Add more optimization levels

--- PORT/src/rust/config.rs
+++ PORT/src/rust/config.rs
@@ -107,6 +107,12 @@ enum OptimizationLevel {
     One = 1,
     Two = 2,
     Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
 }
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
