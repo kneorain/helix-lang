7bf35bd4e1759e4ca66ceedc81ed132476b4fe60
Dhruvan <dhruvan.k05@gmail.com>
2024-03-27 13:50:37 -0400

    Update dependencies and add tokio crate

--- PORT/src/rust/token/lexer.rs
+++ PORT/src/rust/token/lexer.rs
@@ -1,76 +1,91 @@
 use super::{Token, TokenType};
-use crate::rust::token::token_value::TokenValue;
 use regex::Regex;
 use std::sync::Arc;
-use std::{fs::File, io::{self, BufRead}, path::Path};
+use std::{fs::File, io::self};
+use std::sync::Mutex;
+use std::thread;
+use std::sync::mpsc;
+use std::io::BufRead;
+
 
 pub struct Lexer {
     file_name: Arc<str>,
-    string_re: Regex,
-    character_re: Regex,
-    numeric_re: Regex,
-    identifier_re: Regex,
-    delimiter_re: Regex,
-    operator_re: Regex,
-    comment_re: Regex,
 }
 
+lazy_static::lazy_static! {
+    static ref STRING_RE: Regex = Regex::new(r#"([fbur]*"[^"\\]*(?:\\.[^"\\]*)*")"#).unwrap();
+    static ref CHARACTER_RE: Regex = Regex::new(r#"([fbur]*'[^'\\]*(?:\\.[^'\\]*)*')"#).unwrap();
+    static ref NUMERIC_RE: Regex = Regex::new(r#"(\b\d+\.\d+\b|\b\d+[fdui]?\b)"#).unwrap();
+    static ref IDENTIFIER_RE: Regex = Regex::new(r#"(\b[a-zA-Z][a-zA-Z0-9_]*\b)"#).unwrap();
+    static ref DELIMITER_RE: Regex = Regex::new(r#"([\(\)\{\}\[\];,])"#).unwrap();
+    static ref OPERATOR_RE: Regex = Regex::new(r#"([+\-*/%=&|!<>^])"#).unwrap();
+    static ref COMMENT_RE: Regex = Regex::new(r#"((?://[^\n]*)|(/\*[\s\S]*?\*/))"#).unwrap();
+}
 
-/*
-COMPILED_RE = re.compile(rf"""
-            ([fbur]*"[^"\\]*(?:\\.[^"\\]*)*")                                                                          | # Double quotes strings, including f, b, r, u strings
-            ([fbur]*'[^'\\]*(?:\\.[^'\\]*)*')                                                                          | # Single quotes strings, including f, b, r, u strings
-            ({back_slash.join(base.COMMENT.split())}[^\n]*)                                                         | # Single line comments (~~)
-            ({back_slash.join(base.BLOCK_COMMENT.split())}[\s\S]*?{back_slash.join(base.BLOCK_COMMENT.split())}) | # Multi line comments (~*~ ... ~*~)
-            (\b\d+\.\d+\b)                                                                                             | # Decimal numbers
-            (\b\w+\b)                                                                                                  | # Words (identifiers, keywords)
-            ({'|'.join(base.FAT_CHARACTER)})                                                                        | # Double character operators
-            ([\(\){{}};,])                                                                                             | # Single character delimiters
-            (\S)                                                                                                       | # Catch other characters
-        """, re.MULTILINE | re.VERBOSE)
-
-        
-*/
-
-// TODO: Put regexps in a lazy static
+// make an unsafe but VERY fast io reader for the lexer
 
 impl Lexer {
     pub fn new(file_name: &str) -> Self {
         Self {
             file_name: Arc::from(file_name.to_owned()),
-            string_re: Regex::new(r#"([fbur]*"[^"\\]*(?:\\.[^"\\]*)*")"#).unwrap(),
-            character_re: Regex::new(r#"([fbur]*'[^'\\]*(?:\\.[^'\\]*)*')"#).unwrap(),
-            numeric_re: Regex::new(r#"(\b\d+\.\d+\b|\b\d+[fdui]?\b)"#).unwrap(),
-            identifier_re: Regex::new(r#"(\b[a-zA-Z][a-zA-Z0-9_]*\b)"#).unwrap(),
-            delimiter_re: Regex::new(r#"([\(\)\{\}\[\];,])"#).unwrap(),
-            operator_re: Regex::new(r#"([+\-*/%=&|!<>^])"#).unwrap(),
-            comment_re: Regex::new(r#"((?://[^\n]*)|(/\*[\s\S]*?\*/))"#).unwrap(),
         }
     }
 
-    pub fn tokenize(&self) -> io::Result<Vec<Token>> {
-        let file = File::open(Path::new(self.file_name.as_ref()))?;
+    pub fn tokenize(&self) -> Vec<Token> {
+        let file = File::open(&*self.file_name).unwrap();
         let reader = io::BufReader::new(file);
-        let mut tokens = Vec::new();
+        let tokens = Arc::new(Mutex::new(Vec::new()));
+        let mut line_number = 1;
+    
+        let (sender, receiver) = mpsc::channel();
+    
+        // Spawn multiple threads to process lines concurrently
+        let num_threads = 4;
+        let receiver = Arc::new(Mutex::new(receiver));
 
-        for (line_number, line) in reader.lines().enumerate() {
-            let line: Arc<str> = Arc::from(line.unwrap_or_default().as_str().to_owned());
-            let line_tokens = self.tokenize_line(line, line_number as i32);
-            tokens.extend(line_tokens);
+        for _ in 0..num_threads {
+            let tokens = Arc::clone(&tokens);
+            let file_name = Arc::clone(&self.file_name);
+            let receiver = Arc::clone(&receiver);
+
+            let handle = thread::spawn(move || {
+                while let Ok(line) = receiver.lock().unwrap().recv() {
+                    let line_tokens = Lexer::tokenize_line(file_name.clone(), Arc::from(line), line_number);
+                    line_number += 1;
+
+                    tokens.lock().unwrap().extend(line_tokens);
+                }
+            });
+
+            handle.join().unwrap();
         }
 
-        Ok(tokens)
+        // Read the file line by line and send each line to the threads
+        for line in reader.lines() {
+            sender.send(line.unwrap()).unwrap();
+        }
+
+        // Drop the sender to close the channel
+        drop(sender);
+
+        // Wait for all threads to finish
+        for _ in 0..num_threads {
+            receiver.lock().unwrap().recv().unwrap();
+        }
+
+        // Extract the tokens from the Arc<Mutex<Vec<Token>>>
+        Arc::try_unwrap(tokens).unwrap().into_inner().unwrap()
     }
 
-    fn tokenize_line(&self, line: Arc<str>, line_number: i32) -> Vec<Token> {
+    fn tokenize_line(file_name: Arc<str>, line: Arc<str>, line_number: i32) -> Vec<Token> {
         let mut tokens = Vec::new();
         let mut start = 0;
 
         while start < line.len() {
-            if let Some((length, token_type)) = self.find_next_token(&line[start..]) {
+            if let Some((length, token_type)) = Lexer::find_next_token(&line[start..]) {
                 tokens.push(Token::new(
                     line.clone(),
-                    self.file_name.clone(),
+                    file_name.clone(),
                     line_number,
                     start as u16,
                     line[start..start + length].trim().to_owned(),
@@ -85,23 +100,35 @@ impl Lexer {
         return tokens;
     }
 
-    fn find_next_token(&self, s: &str) -> Option<(usize, TokenType)> {
-        let regexps = [
-            (&self.string_re, TokenType::STRING),
-            (&self.character_re, TokenType::CHARACTER),
-            (&self.numeric_re, TokenType::NUMERIC),
-            (&self.identifier_re, TokenType::IDENTIFIER),
-            (&self.delimiter_re, TokenType::DELIMITER),
-            (&self.operator_re, TokenType::OPERATOR),
-            (&self.comment_re, TokenType::COMMENT),
-        ];
-
-        for (regex, token_type) in regexps {
-            if let Some(matched) = regex.find(s) {
-                return Some((matched.end(), token_type));
-            }
+    fn find_next_token(s: &str) -> Option<(usize, TokenType)> {
+        if let Some(captures) = STRING_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::STRING));
         }
 
-        None
+        if let Some(captures) = CHARACTER_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::CHARACTER));
+        }
+
+        if let Some(captures) = NUMERIC_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::NUMERIC));
+        }
+
+        if let Some(captures) = IDENTIFIER_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::IDENTIFIER));
+        }
+
+        if let Some(captures) = DELIMITER_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::DELIMITER));
+        }
+
+        if let Some(captures) = OPERATOR_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::OPERATOR));
+        }
+
+        if let Some(captures) = COMMENT_RE.captures(s) {
+            return Some((captures.get(0).unwrap().as_str().len(), TokenType::COMMENT));
+        }
+
+        return None;
     }
 }
