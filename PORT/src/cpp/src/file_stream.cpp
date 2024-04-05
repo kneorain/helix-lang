1b7db90426bab97dd6c5272b2e130e8501a24f6b
Dhruvan <dhruvan.k05@gmail.com>
2024-04-05 03:48:43 -0400

    tested on windows, new file_stream.cpp takes 200ns + ~overhead for moving data between c++ to rust to read any line or a range of lines from a file.

--- PORT/src/cpp/src/file_stream.cpp
+++ PORT/src/cpp/src/file_stream.cpp
@@ -81,11 +81,14 @@
 
 #include "helix-compiler/src/cpp/mod.rs.h"
 #include "rust/cxx.h"
+
 // headers
 #include <cstdint>
+#include <iostream>
 #include <memory>
-#include <algorithm>
+#include <sstream>
 #include <cstring>
+
 // mmap
 #include <string>
 #include <sys/stat.h>
@@ -100,155 +103,150 @@
 
 #define MAX_LINE_CHUNKS 0x1e
 
-static inline size_t rls(char* data, int startLine, int endLine) {
-    if (startLine > endLine || startLine < 1) return 0;
 
-    const char* cursor = data;
-    size_t size = 0;
-    int currentLine = 1;
+struct FileInfo {
+    uint32_t              total_lines;
+    std::vector<uint32_t> line_starts ;
+};
 
-    // skip lines until it reaches the startLine
-    while (*cursor && currentLine < startLine) {
-        if (*cursor++ == '\n') {
-            currentLine++;
+template<class init>
+FileInfo count(const init first, const init last, const char val) {
+    std::_Adl_verify_range(first, last);
+    auto usize_first      = std::_Get_unwrapped(first);
+    const auto usize_last = std::_Get_unwrapped(last);
+
+    std::vector<uint32_t> line_starts = {};
+    for (; usize_first != usize_last; ++usize_first) {
+        if (*usize_first == val) {
+            line_starts.push_back(std::distance(first, usize_first));
         }
     }
 
-    // count bytes in the specified line range
-    while (*cursor && currentLine <= endLine) {
-        size += (*cursor != '\n');
-        currentLine += (*cursor++ == '\n');
-    }
-
-    return size;
-}
-
-// function to read a specific line from a file
-static inline std::string rl(char*& data, int line) {
-    if (--line < 0) return ""; // adjust for 0-based indexing
-
-    const char* line_start = nullptr;
-    const char* cursor = data;
-
-    for (; *cursor; ++cursor) {
-        if (line_start == nullptr) line_start = cursor; // set start at the beginning or after a newline
-
-        if (*cursor == '\n') {
-            if (line-- == 0) {
-                // found the desired line
-                return std::string(line_start, cursor - line_start);
-            }
-            line_start = nullptr; // reset start for next line
-        }
-    }
-
-    // handle the case where the last line doesn't end with a newline
-    return (line == 0) ? std::string(line_start, cursor - line_start) : std::string();
-}
-
-static long long tl(char const *fname) {
-    #ifdef __unix__ // tell kernel the access pattern.
-        posix_fadvise(fd, 0, 0, 1);  // FDADVICE_SEQUENTIAL
-    #endif
-
-    // memory map the file
-    #ifdef __unix__
-        struct stat sb;
-        size_t size = sb.st_size;
-        fstat(
-            fileno(fd),
-            &sb
-        );
-        char* data = (char*) mmap(
-            NULL,
-            size,
-            PROT_READ,
-            MAP_PRIVATE,
-            fileno(fd),
-            0
-        );
-    #elif _WIN32
-        HANDLE hFile = CreateFileA(
-            fname,
-            GENERIC_READ,
-            FILE_SHARE_READ,
-            NULL,
-            OPEN_EXISTING,
-            FILE_ATTRIBUTE_NORMAL,
-            NULL
-        );
-        HANDLE hMap  = CreateFileMapping(
-            hFile,
-            NULL,
-            PAGE_READONLY,
-            0,
-            0,
-            NULL
-        );
-        DWORD  size  = GetFileSize(
-            hFile,
-            NULL
-        );
-        char*  data  = (char*) MapViewOfFile(
-            hMap,
-            FILE_MAP_READ,
-            0,
-            0,
-            size
-        );
-    #endif
-
-    // get the total number of lines
-    long long total_lines = std::count(
-        data,
-        data + size,
-        '\n'
-    );
-
-    // calculate the number of chunks
-    auto chunks = total_lines / MAX_LINE_CHUNKS;
-
-    if (total_lines % MAX_LINE_CHUNKS != 0) {
-        chunks += 1;
-    }
-
-    // store the following {int, int, int, ...} where each int is the start pos
-    // of a line, and the index is the line number
-
-    #ifdef __unix__ // cleanup
-        munmap(data, size);
-    #elif _WIN32
-        UnmapViewOfFile(data);
-        CloseHandle    (hMap);
-        CloseHandle    (hFile);
-    #endif
-
-    return total_lines;
+    return FileInfo {static_cast<uint32_t>(line_starts.size()), std::move(line_starts)};
 }
 
 namespace file_stream_cpp {
-    FileStream_CPP::FileStream_CPP(const std::string &filename):
-        fileName(filename.c_str()) {
+    FileStream_CPP::FileStream_CPP(const std::string &filename)
+        : fileName(filename.c_str()
+    ) {
+        #ifdef __unix__ // tell kernel the access pattern.
+            posix_fadvise(fd, 0, 0, 1);  // FDADVICE_SEQUENTIAL
+        #endif
+
+        #ifdef __unix__ // memory map the file to unix
+            stat sb;
+            size = sb.st_size;
+            fstat(
+                fileno(fd),
+                &sb
+            );
+            *data = (char*) mmap(
+                NULL,
+                size,
+                PROT_READ,
+                MAP_PRIVATE,
+                fileno(fd),
+                0
+            );
+        #elif _WIN32 // memory map the file to windows
+            hFile = CreateFileA(
+                fileName,
+                0x80000000L, // GENERIC_READ
+                0x00000001L,     // FILE_SHARE_READ
+                NULL,   // default security
+                0x3,   // OPEN_EXISTING
+                0x80,   // FILE_ATTRIBUTE_NORMAL
+                NULL           // no template
+            );
+            hMap  = CreateFileMapping(
+                hFile,
+                NULL,
+                PAGE_READONLY,
+                0,
+                0,
+                NULL
+            );
+            size  = GetFileSize(
+                hFile,
+                NULL
+            );
+            data = (char*) MapViewOfFile(
+                hMap,
+                FILE_MAP_READ,
+                0,
+                0,
+                size
+            );
+        #endif /* count the number of lines in the file */
+        FileInfo info = count(data, data + size, '\n');
+        totalLines = info.total_lines; // store the total number of lines
+        lineStarts = info.line_starts; // store the line starts
     }
 
-    /*
-    4kb of data as a rust::Str ~ 2us
-    reading 4kb of data from a file ~ 8.0us
-    */
+    rust::Str FileStream_CPP::read_line(uint32_t lineIndex) const {
+        if (lineIndex >= totalLines) {
+            return rust::Str(
+                "FAILED TO READ! LINE INDEX OUT OF BOUNDS! GOT: "
+                + std::to_string(lineIndex)
+                + " MAX: "
+                + std::to_string(totalLines)
+            );
+        }
+        
+        uint32_t start = (lineIndex == 0 ? 0 : lineStarts[lineIndex - 1] + 1);
 
-    rust::Str FileStream_CPP::get_data_from_chunk(uint32_t chunkIndex) const {
-        return rust::Str("Total Lines = " + std::to_string(tl(fileName)));
+        return rust::Str(
+            data + start,
+            lineStarts[lineIndex] - start
+        );
+    }
+
+    rust::Str FileStream_CPP::read_lines(uint32_t startLine, uint32_t offset) const {
+        if (startLine >= totalLines || (startLine + offset) >= totalLines) {
+            return rust::Str(
+                "FAILED TO READ! LINE INDEX OUT OF BOUNDS! GOT: "
+                + std::to_string(startLine)
+                + " MAX: "
+                + std::to_string(totalLines)
+            );
+        }
+
+        uint32_t start = (startLine == 0 ? 0 : lineStarts[startLine - 1] + 1);
+
+        return rust::Str(
+            data + start,
+            lineStarts[startLine + offset] - start
+        );
+    }
+
+    rust::Str FileStream_CPP::read_file() const {
+        return rust::Str(data, size);
+    }
+
+
+    rust::Str FileStream_CPP::get_file_name() const {
+        return rust::Str(fileName);
+    }
+
+    uint32_t FileStream_CPP::get_total_lines() const {
+        return totalLines;
     }
 
     FileStream_CPP::~FileStream_CPP() {
-        //fileStream.close();
-    }
-
-    uint32_t FileStream_CPP::get_total_chunks() const {
-        return 1;
+        #ifdef __unix__ // cleanup
+            munmap(data, size);
+        #elif _WIN32
+            UnmapViewOfFile(data);
+            CloseHandle(hMap);
+            CloseHandle(hFile);
+        #endif
+        // deallocate memory
+        delete[] data;
+        
     }
 
     std::unique_ptr<FileStream_CPP> new_file_stream(rust::Str filename) {
         return std::make_unique<FileStream_CPP>(std::string(filename));
     }
+
 }
--- PORT/src/main.rs
+++ PORT/src/main.rs
@@ -68,40 +68,25 @@ fn main() -> io::Result<()> {
     println!("\n---------- RUST ----------");
     let cwd = std::env::current_dir().unwrap();
     println!("Current working directory: {}", cwd.display());
-    
-    //let inst = cpp::file_stream::new_file_stream("PORT/src/test.hlx");
-    
-    
-    
-    //let _reader = inst.get_data_from_chunk(1);
-    
-    //let inst = cpp::file_stream::new_file_stream("PORT/src/test.hlx");
-    //let _reader = inst.get_data_from_chunk(0);
-    
-    // let start = time::Instant::now();
-    
-    // let input = File::open("PORT/src/copy.hlx")?;
-    // let buffered = std::io::BufReader::new(input);
-    // let line_count = buffered.lines().count();
-    
-    // let elapsed = start.elapsed();
-    
-    // println!("Rust IO Elapsed: {:?}", elapsed);
-    // println!("total lines {}", line_count);
-    
-    // FIXME: C++ IS UNDER COUNTING LINES
-    
+
     println!("\n---------- C++ ----------");
+    let inst = cpp::file_stream::new_file_stream("PORT/src/copy.hlx");
+    
     let start = time::Instant::now();
     
-    let inst = cpp::file_stream::new_file_stream("PORT/src/copy.hlx");
-    let line_count = inst.get_data_from_chunk(0);
+    let lines = inst.read_file();
+    let lines2 = inst.read_line(2187622);
+    let lines3 = inst.read_lines(1, 5);
     
     let elapsed = start.elapsed();
+    println!("file: {}", lines);
+    println!("line 1 {}", lines2);
+    println!("lines 1-5 {}", lines3);
     println!("C++ IO Elapsed: {:?}", elapsed);
-    println!("{}", line_count);
 
-    thread::sleep(time::Duration::from_millis(200));
+
+    
+    thread::sleep(time::Duration::from_millis(500));
 
     //println!("{}", _reader);
     //let lexar = rust::token::lexer::Lexer::new("PORT/src/test.hlx");
