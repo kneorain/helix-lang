b0f798042d8bda4a16ad0aab054c920a8b42d43a
Dhruvan <dhruvan.k05@gmail.com>
2024-04-09 18:54:27 -0400

    reworking the file_reader

--- PORT/src/cpp/include/file_reader.hpp
+++ PORT/src/cpp/include/file_reader.hpp
@@ -1,84 +1,8 @@
 #pragma once
-/** ----------------------------------------------------------------------------
- * @link{https://github.com/kneorain/helix-lang}
- *
- * @brief This file is part of the Helix Compiler, an open-source compiler
- *        for the Helix programming language.
- *
- * @author Kneorain
- * @date 2024
- *
- * This work is licensed under the Creative Commons Attribution 4.0
- * International License. To view a copy of this license, visit
- * http://creativecommons.org/licenses/by/4.0/ or send a letter to
- * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
- * @see http://creativecommons.org/licenses/by/4.0/
- *
- * You are free to:
- * - Share — copy and redistribute the material in any medium or format
- * - Adapt — remix, transform, and build upon the material
- * for any purpose, even commercially, under the following terms:
- * - Attribution — You must give appropriate credit, provide a link to
- *   the license, and indicate if changes were made. You may do so in
- *   any reasonable manner, but not in any way that suggests the licensor
- *   endorses you or your use.
- *
- * This is a human-readable summary of (and not a substitute for) the license.
- * For the full license text, see the LICENSE file in the root directory of
- * this source code repository or visit the URL above.
- *
- * @author Dhruvan Kartik
- *
- * -----------------------------------------------------------------------------
- *
- * @file file_stream.cpp
- * Description: This file forms an integral part of the frontend for the Helix
- * Compiler. It includes the implementation of the T_FileReader class, which
- * serves as a pivotal component in handling large files efficiently. The
- * primary functionality of this class is to perform memory mapping for rapid
- * file access and ensure thread-safe operations for concurrent data reading.
- * This is particularly important in the context of a compiler where speed and
- * efficiency of file processing directly impact the overall compilation time.
- *
- * The T_FileReader class is designed to optimize the file reading process,
- * leveraging multithreading to enhance performance. It intelligently divides
- * the file into manageable chunks and enables simultaneous processing by
- * multiple threads. This approach is not only efficient but also scalable,
- * accommodating the varying demands of different file sizes and system
- * capabilities.
- *
- * Usage in Compiler Workflow:
- * - The main thread initiates the process by creating a T_FileReader object.
- * This object becomes the gateway to the underlying file handling operations.
- * - The total number of chunks in the file is determined by invoking the
- * get_total_chunks() method on the T_FileReader object.
- * - Based on this line division, the program dynamically allocates threads,
- * each responsible for processing a subset of the total chunks.
- * - Each thread, in its lifecycle, calls the get_data_from_chunk() method for
- * each line it is assigned, thereby enabling parallel processing of the file
- * data.
- *
- * Important Considerations:
- * - T_FileReader has been designed with thread safety in mind. However, it is
- * crucial that the implementation is carefully managed to avoid race conditions
- * and ensure data integrity.
- * - It is imperative that no two threads access the same line simultaneously.
- * Proper synchronization mechanisms or access control should be in place to
- * enforce this rule.
- * - The design and implementation should be robust to handle any exceptions or
- * edge cases that might arise during file operations, ensuring stability and
- * reliability of the compiler.
- *
- * The development of this file and its functionality has been guided by the
- * overarching goal to enhance the performance and reliability of the Helix
- * Compiler. Through effective file management and parallel processing
- * capabilities, it contributes significantly to the compiler's efficiency,
- * especially when dealing with large source files.
- *
- * @since   v0.0.1-stable
- * @version v0.1.2-stable
- * -------------------------------------------------------------------------- */
+
 #include "rust/cxx.h"
+#include <atomic>
+#include <climits>
 #include <memory>
 #include <cstdint>
 #include <vector>
@@ -96,88 +20,111 @@
  * @brief file_stream_cpp namespace provides functionality for reading data
  *        from a file in chunks. also thread safe. (**soon**)
  */
-namespace file_reader {
-    class T_FileReader {
+namespace file_io {
+    class FileIO {
         public:
-            /**
-             * @brief this class provides functionality for reading data
-             *        from a file in an highly optimized manner for a compiler.
-             *
-             * constructs a T_FileReader object with the specified filename.
-             * does the following magic:
-             * - calculates the total number of lines in the file.
-             * - calculates the size of the file.
-             * - calculates the number of chunks in the file.
-             * - stores the start pos of each line in the chunks vector.
-             * all done in > 500µs for a 40mb (2M lines - non empty) file.
-             * **Tested on my machine**
-             *
-             * @param filename The name of the file to be read.
-             */
-             T_FileReader(const std::string &filename);
-            ~T_FileReader();
+            virtual ~FileIO() {}
 
-            /**
-             * retrieves data from the specified line index.
-             *
-             * @param chunkIndex The index of the line to retrieve data from.
-             * @return The data from the specified line as a rust::Str.
-             */
-            rust::Str read_line(uint32_t lineIndex) const;
-            
-            /**
-             * retrieves data from the specified line index with an offset.
-             *
-             * @param startLine The index of the line to start reading from.
-             * @param offset The number of lines to read.
-             * @return The data from the specified line as a rust::Str.
-             */
-            rust::Str read_lines(uint32_t startLine, uint32_t offset) const;
+            static std::shared_ptr<FileIO> open(const rust::Str &filename,
+                                                const rust::Str &mode = "r",
+                                                const rust::Str &encoding = "utf-8");
 
-            /**
-             * retrieves the entire file as a rust::Str.
-             *
-             * @return The entire file as a rust::Str.
-             */
-            rust::Str read_file() const;
-            
-            /**
-             * retrieves the total number of chunks in the file.
-             *
-             * @return The total number of chunks.
-             */
-            uint32_t get_total_lines() const;
+            virtual rust::slice<const uint8_t> readLine() = 0;
+            virtual rust::slice<const uint8_t> readLines() = 0;
+            virtual rust::slice<const uint8_t> read() = 0;
 
-            /**
-             * retrieves the name of the file.
-             *
-             * @return The name of the file.
-             */
-            rust::Str get_file_name() const;
+            virtual void write(const rust::slice<const uint8_t> &data) = 0;
+            virtual void writeLine(const rust::slice<const uint8_t> &data) = 0;
+            virtual void writeLines(const rust::slice<const rust::slice<const uint8_t>> &lines) = 0;
 
-        private:
-            const char    *fileName;
-            uint32_t       totalLines;
-            mutable std::vector<uint32_t> lineStarts;
+            virtual void append(const rust::slice<const uint8_t> &data) = 0;
+            virtual void appendLine(const rust::slice<const uint8_t> &data) = 0;
+            virtual void appendLines(const rust::slice<const rust::slice<const uint8_t>> &lines) = 0;
 
-            #if defined __unix__ || __APPLE__
-                size_t size;
-                char  *data;
-            #elif _WIN32
-                DWORD  size;
-                HANDLE hMap;
-                HANDLE hFile;
-                char  *data;
-            #endif
+            virtual void close() = 0;
+
+        protected:
+            FileIO() = default;
     };
 
-    /**
-     * creates a new T_FileReader object with the specified filename.
-     * this function is the entry point for creating T_FileReader objects.
-     * to use in rust/c code.
-     *
-     * @param filename The name of the file to be read.
-     * @return A unique pointer to the newly created T_FileReader object.
-     */
-    std::unique_ptr<T_FileReader> init(rust::Str filename);
+    class FileIO_R : public FileIO {
+             FileIO_R(const rust::Str &filename, const rust::Str &encoding);
+            ~FileIO_R();
+
+            rust::slice<const uint8_t> readLine();
+            rust::slice<const uint8_t> readLines();
+            rust::slice<const uint8_t> read();
+
+            std::atomic_bool is_open;
+            std::atomic_bool is_error;
+            std::atomic_bool is_empty;
+    };
+
+
+    class FileIO_W : public FileIO {
+         FileIO_W(const rust::Str &filename, const rust::Str &encoding);
+        ~FileIO_W();
+
+        void write(const rust::Str &data);
+        void writeLine(const rust::Str &data);
+        void writeLines(const rust::slice<const rust::Str> &lines);
+
+        std::atomic_bool is_open;
+        std::atomic_bool is_error;
+    };
+
+    class FileIO_A : public FileIO {
+         FileIO_A(const rust::Str &filename, const rust::Str &encoding);
+        ~FileIO_A();
+
+        void append(const rust::Str &data);
+        void appendLine(const rust::Str &data);
+        void appendLines(const rust::slice<const rust::Str> &lines);
+
+        rust::slice<const uint8_t> readLine();
+        rust::slice<const uint8_t> readLines();
+        rust::slice<const uint8_t> read();
+
+        std::atomic_bool is_open;
+        std::atomic_bool is_error;
+        std::atomic_bool is_empty;
+    };
+
+
+    class FileIO_Internal {
+        private:
+            FileIO **files;
+            static FileIO_Internal *instance;
+
+            FileIO_Internal() {
+                files = static_cast<FileIO**>(malloc(sizeof(FileIO*) * 0xf36db));
+            }
+
+        public:
+            ~FileIO_Internal() {
+                delete[] files;
+            }
+
+            static FileIO_Internal* getInstance() {
+                if (!instance) {
+                    instance = new FileIO_Internal();
+                }
+                return instance;
+            }
+    };
+
+    inline FileIO_Internal* FileIO_Internal::instance = nullptr;
+    inline std::shared_ptr<FileIO> FileIO::open(const rust::Str &filename,
+                                                const rust::Str &mode,
+                                                const rust::Str &encoding
+    ) {
+        if (mode == "r") {
+            return std::make_shared<FileIO_R>(filename, encoding);
+        } else if (mode == "w") {
+            return std::make_shared<FileIO_W>(filename, encoding);
+        }
+
+        throw std::invalid_argument("Unsupported file mode");
+    }
+    void init();
 }
