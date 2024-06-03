49e530625554cab0ca71acd24464a46bdf5ac2b7
Dhruvan <dhruvan.k05@gmail.com>
2024-06-03 23:43:55 +0400

    chore: Remove unused files and directories, restructure project for better organization and debugging

--- xmake.lua
+++ xmake.lua
@@ -1,45 +1,66 @@
 set_project("helix-lang")
+add_rules  ("mode.debug", "mode.release")
 
-add_rules("mode.debug", "mode.release")
+----------------------------------------------------------------------------------------------------
 
 target("helix-lang")
     set_kind("binary")
-    add_files("src/*.cc")
-    -- set the standard c++ version to c++2b
-    set_languages("c++2b")
-    set_optimize("fastest")
-    -- enable multi-thread compilation
+    set_warnings("all")
+    
+    add_files("source/*.cc")    -- add all files in the source directory
+    add_files("source/**/*.cc") -- add all files in the source directory's subdirectories
+    
+    set_languages("c++2b")      -- set the standard c++ version to c++2b
+    set_optimize ("fastest")
+    
     add_includedirs("/opt/llvm-aarch64/include")
-    add_linkdirs("/opt/llvm-aarch64/lib")
-    add_cxxflags("-stdlib=libc++", "-fno-rtti", "-I/opt/llvm-aarch64/include")
-    add_ldflags("-L/opt/llvm-aarch64/lib", "-lc++abi", "-Wl,-rpath,/opt/llvm-aarch64/lib")
-    -- explicitly add the LLVM libraries
+    add_linkdirs   ("/opt/llvm-aarch64/lib")
+    
+    add_links("LLVM-18", "clang", "clang-cpp", "c++", "c++abi") -- explicitly add LLVM libraries
+    
+    add_cxxflags("-stdlib=libc++"         , "-fno-rtti", "-I/opt/llvm-aarch64/include"     )
+    add_ldflags ("-L/opt/llvm-aarch64/lib", "-lc++abi" , "-Wl,-rpath,/opt/llvm-aarch64/lib")
+
+----------------------------------------------------------------------------------------------------
+
+target("test")
+    set_kind("binary")
+    set_warnings("all")
+    
+    add_files("test/*.cc")
+    add_files("test/**/*.cc")
+    
+    set_languages("c++2b")
+    set_optimize ("fastest")
+    
+    add_includedirs("/opt/llvm-aarch64/include")
+    add_linkdirs   ("/opt/llvm-aarch64/lib")
+    
     add_links("LLVM-18", "clang", "clang-cpp", "c++", "c++abi")
+    
+    add_cxxflags("-stdlib=libc++"         , "-fno-rtti", "-I/opt/llvm-aarch64/include"     )
+    add_ldflags ("-L/opt/llvm-aarch64/lib", "-lc++abi" , "-Wl,-rpath,/opt/llvm-aarch64/lib")
 
-before_build(function (target)
-    import("lib.detect.find_tool")
-    local llvm_config = find_tool("llvm-config", {paths = "/opt/llvm-aarch64/bin"})
-    if llvm_config then
-        local llvm_includedir = os.iorunv(llvm_config.program, {"--includedir"}):trim()
-        local llvm_libdir = os.iorunv(llvm_config.program, {"--libdir"}):trim()
-        target:add("includedirs", llvm_includedir)
-        target:add("linkdirs", llvm_libdir)
-    else
-        raise("llvm-config not found!")
-    end
-end)
+----------------------------------------------------------------------------------------------------
+
+before_build(
+    function(target)
+        import("lib.detect.find_tool")
         
+        local llvm_config = find_tool("llvm-config", {paths = "/opt/llvm-aarch64/bin"})
+        
+        if llvm_config then
+            local llvm_includedir = os.iorunv(llvm_config.program, {"--includedir"}):trim()
+            local llvm_libdir     = os.iorunv(llvm_config.program, {"--libdir"}    ):trim()
+            
+            target:add("includedirs", llvm_includedir)
+            target:add("linkdirs"   , llvm_libdir    )
+        
+        else
+            raise("llvm-config not found!")
+        
+        end
+    end
+)
 
--- llvm install instructions for macos (arm64)
--- brew install cmake ninja wget
--- cd ~
--- mkdir -p ~/built-includes/llvm-install
--- cd ~/built-includes/llvm-install
--- wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.6/llvm-project-18.1.6.src.tar.xz
--- tar -xvf llvm-project-18.1.6.src.tar.xz
--- cd llvm-project-18.1.6.src
--- mkdir build
--- cd build
--- cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind" -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="AArch64" -DCMAKE_INSTALL_PREFIX=/opt/llvm-aarch64 ../llvm
--- ninja
--- sudo ninja install
+----------------------------------------------------------------------------------------------------
